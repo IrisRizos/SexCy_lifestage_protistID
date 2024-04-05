8f5a09604ee68bb427487208c3a5d7f6affcb0ab
IrisRizos <92450816+IrisRizos@users.noreply.github.com>
2024-04-05 10:48:23 +0200

    Update and rename fff to eggNOG_fun.sh

---
+++ Preliminary_analysis/eggNOG_fun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#SBATCH --job-name eggnog
+#SBATCH --cpus-per-task=12
+#SBATCH --mem 50G
+#SBATCH -o o.egg.%N.%j
+#SBATCH -e e.egg.%N.%j
+#SBATCH --mail-user=iris.rizos@sb-roscoff.fr
+#SBATCH --mail-type=ALL
+
+module load eggnog-mapper/2.1.11
+
+today=$(date +%F)
+
+for f in /shared/projects/rhizaria_ref/finalresult/Sexual_cycle/adult/Acantharia/GC*.pep;
+do
+  echo "${f##*/}"
+emapper.py -i ${f} --itype proteins -m diamond --dmnd_algo auto -o ${f##*/}_${today} --evalue 0.001 --dbmem --cpu 12 --data_dir /shared/bank/eggnog-mapper/current/ --override
+done
+
+##
