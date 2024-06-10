e97cbb33396c34183dafd3f6838c932a5a38cc2d
IrisRizos <92450816+IrisRizos@users.noreply.github.com>
2024-06-10 12:25:23 +0200

    Update ortho_search.sh

--- Comparative_approach/ortho_search.sh
+++ Comparative_approach/ortho_search.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name orthof
 #SBATCH --cpus-per-task=4
-#SBATCH -o o.orthof_%N%j
-#SBATCH -e e.orthof_%N%j
-#SBATCH --mail-user=iris.rizos@sb-roscoff.fr
+#SBATCH -o o.orthof_%N.%j
+#SBATCH -e e.orthof_%N.%j
+#SBATCH --mail-user=
 #SBATCH --mail-type=BEGIN,FAIL,END
 
 # Module
 module load orthofinder/2.5.2
 
 # Command
-orthofinder -f /shared/projects/swarmer_radiolaria/finalresult/ortholog/Acantharia/
+orthofinder -f /[path to folder with predicted proteomes]/
