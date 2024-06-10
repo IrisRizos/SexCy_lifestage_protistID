dfa11ca7f7f2aae2827765b68ddd4b78837cc4d8
IrisRizos <92450816+IrisRizos@users.noreply.github.com>
2024-06-10 11:42:38 +0200

    Update remap_expr.sh

--- Preliminary_analysis/remap_expr.sh
+++ Preliminary_analysis/remap_expr.sh
@@ -1,26 +1,29 @@
-#!/bin/sh
-#
-#SBATCH --job-name genexp
-#SBATCH --cpus-per-task=6
-#SBATCH -o o.genexp2
-#SBATCH -e e.genexp2
-#SBATCH --mail-user=iris.rizos@sb-roscoff.fr
-#SBATCH --mail-type=BEGIN,FAIL,END
+#!/bin/bash
+#SBATCH -p fast
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --cpus-per-task 8
+#SBATCH --job-name=salmon_rate_remapping
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=
+#SBATCH -o log/rate_remapping_ADNU-71-1_hard.out
+#SBATCH -e log/rate_remapping_ADNU-71-1_hard.err
 
-# trinity/2.13.2
-# perl/5.26.2
+module load trinity/2.13.2
 
-today=$(date +%F)
+WORKING_PATH="/shared/projects/swarmer_radiolaria/"
+sample="ADNU-71-1"
+R1="${WORKING_PATH}input/www.fasteris.com/private/clean/ADNU-71-1_prinseq.fastq_1.fastq.gz"
+R2="${WORKING_PATH}input/www.fasteris.com/private/clean/ADNU-71-1_prinseq.fastq_2.fastq.gz"
 
-# For every vegetative file
-for f in /shared/projects/swarmer_radiolaria/finalresult/rate_remapping/*.log;
-do
-   srun abundance_estimates_to_matrix.pl \
-   --est_method salmon \
-   --out_prefix Matrix_genexpr_${f##*/}_${today}_ \
-   --name_sample_by_basedir \
-   --gene_trans_map none \
-   ${f}/quant.sf \
-done
+## /!\ Careful about the symbolic link before running the script
 
+srun align_and_estimate_abundance.pl \
+        --transcripts ${WORKING_PATH}finalresult/rate_remapping/${sample}_hard_filtered_transcripts.fasta \
+        --seqType fq \
+        --left ${R1} \
+        --right ${R2} \
+        --est_method salmon \
+        --trinity_mode \
+        --prep_reference --thread_count 8 \
+        --output_dir ${WORKING_PATH}tmp/rate_remapping/salmon_align_and_estimate_abundance_${sample}_hard.log
 ##
