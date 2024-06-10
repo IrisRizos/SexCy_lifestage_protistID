a5014bdba2cd8d7fadea5a74866b437d4ddd7e8c
IrisRizos <92450816+IrisRizos@users.noreply.github.com>
2024-06-10 12:05:35 +0200

    Update and rename phylogeny_AlignTrim.sh to phylogeny_AlignTrim.sh

--- 1. Background_analysis/phylogeny_AlignTrim.sh
+++ 1. Background_analysis/phylogeny_AlignTrim.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH -o o.align.%N.%j
 #SBATCH -e e.align.%N.%j
-#SBATCH --mail-user=iris.rizos@sb-roscoff.fr
+#SBATCH --mail-user=
 #SBATCH --mail-type=BEGIN,FAIL,END
 
 # Load modules
@@ -12,15 +12,15 @@ module load mafft/7.515
 module load trimal/1.4.1
 
 # File
-file="V4_sc_ref_0804_Acanth.fasta"
+file="[V4 barcode file].fasta"
 
-# Make sure file does not contain special characters
+# Make sure file does not contain special characters (some phylogenetic reconstruction tools don't like them)
 sed -i 's/:/_/g' ${file}
 sed -i 's/(+)/_/g' ${file}
 sed -i 's/(-)/_/g' ${file}
 sed -i 's/;/\n/g' ${file}
 
-# MSA: command for <200 sequences, ~lengths, closely related groups:
+# MSA: command for <200 sequences, similar lengths and closely related protist groups (only acantharian Radiolaria here):
 mafft --maxiterate 1000 --globalpair  ${file} > "aligned_"${file}
 
 #mafft --auto  ${file} > "aligned_"${file}
